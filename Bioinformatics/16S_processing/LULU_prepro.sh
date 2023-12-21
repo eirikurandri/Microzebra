6add7d9d1d30a8b5e9737b761fea868d7f8205c6
eirikurandri <74656937+eirikurandri@users.noreply.github.com>
2023-12-21 13:19:30 +0100

    Update LULU_prepro.sh

--- Bioinformatics/16S_processing/LULU_prepro.sh
+++ Bioinformatics/16S_processing/LULU_prepro.sh
@@ -1,1 +1,3 @@
-
+#do the pre processing neccssary for input into LULU
+makeblastdb -in ASVs.fa -parse_seqids -dbtype nucl
+blastn -db ASVs.fa -outfmt '6 qseqid sseqid pident' -out match_list.txt -qcov_hsp_perc 80 -perc_identity 84 -query ASVs.fa
